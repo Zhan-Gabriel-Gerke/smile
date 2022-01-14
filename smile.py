69a8c4a155e84ec303d81ebe69c00d68795f9c95
opilane <opilane@HP-CZC6297B92>
2022-01-14 14:22:43 +0200

    1

--- smile.py
+++ smile.py
@@ -1,41 +1,44 @@
 from tkinter import *
+
 def lisa_nina():
-    if var_nina.get()=="nina":
-        lbl.configure(text="")
+    if var_nina.get()=="Nina":
+        c.create_oval((135, 147, 165, 183),width=3, fill="#000000", outline="#ffffff") #нос
     elif var_nina.get()=="tühi":
-        lbl.configure(text="On vaja nina kustutada ära")
-def lisa_s():
-    if var_suu.get()=="suu":
-        vastus.configure(text="Lisame suu canva peale")
-    else:
-        vastus.configure(text="On vaja suu kunstutada ära")
+        c.create_oval((135, 135, 165, 165),width=3, fill="#000000", outline="#ffffff") #нос
+def lisa_suu():
+    if var_suu.get()=="Suu":
+        c.create_arc((100, 50, 400,400),start=180,extent=180, style=ARC, fill="#000000",width=10, outline="#000000")
+    elif var_suu.get()=="tühi":
+        c.create_arc((100, 50, 400,400),start=180,extent=180, style=ARC,   fill="#ffffff",width=10, outline="#ffffff")
+def lisa_eyes():
+    if var_eyes.get()=="Silmad":
+        c.create_oval((240, 140, 260, 100), fill="#000000", outline="#000000") #првый 
+        c.create_oval((100, 125, 175, 175), fill="#000000", outline="#000000") #првый 
+    elif var_eyes.get()=="tühi":
+        c.create_oval((250, 125, 400, 200),  fill="#ffffff", outline="#ffffff") #првый 
+        c.create_oval((200, 200, 100, 100),  fill="#ffffff", outline="#ffffff") #левый 
 def lisa_nao():
     if var_nao.get()=="Nägu":
-        c.create_oval((10, 10, 490, 490), fill="#cc99cc", outline="black") #лицо
+        c.create_oval((20, 100, 392, 392),width=5, fill="#ffffff", outline="#000000") #лицо
     elif var_nao.get()=="tühi":
-        c.create_oval((10, 10, 490, 490), fill="#cc99cc", outline="#cc99cc") #лицо
-tk = Tk()
-tk.title("ruuvõrrend")
+        c.create_oval((20, 100, 392, 392),width=5, fill="#ffffff", outline="#000000") #лицо
+aken=Tk()
+aken.title("Face")
+aken.geometry('1000x500')
+aken.configure(bg="#ffffff")
+aken.grab_set()  
+c = Canvas(aken, width=500, height=500, bg="#ffffff")
 var_nina=StringVar()
-f1=Frame(tk,width=500,height=500)
-f1.pack(side=TOP)
-f2=Frame(tk,width=500,height=500)
-f2.pack(side=BOTTOM)
-c=Canvas(tk, width=500, height=500, bg="white")
-c.pack()
-nina=Checkbutton(f1,text = "нос", variable=var_nina,onvalue="nina",offvalue="tühi",command=lisa_nina)
-nina.pack()
-var_s=StringVar()
-suu=Checkbutton(f1,text = "suu",font="Arial 20",command=lisa_s)
-suu.pack()
-c3=Checkbutton(f1,text = "глаза",font="Arial 20",command=...)
-c3.pack()
-c4=Checkbutton(f1,text = "нос",font="Arial 20",command=...)
-c4.pack()
-c5=Checkbutton(f1,text = "рот",font="Arial 20",command=...)
-c5.pack()
-var=IntVar
-c.create_oval((0,0,250,250))
-c.create_oval((0,40,100,100))
-c.create_oval((0,40,100,100))
-tk.mainloop()
+ch_nina=Checkbutton(aken,text="Nina", variable=var_nina, onvalue="Nina", offvalue="tühi",bg="lightsteelblue",command=lisa_nina)
+ch_nina.pack(side=LEFT)
+var_suu=StringVar()
+ch_suu=Checkbutton(aken,text="Suu", variable=var_suu, onvalue="Suu", offvalue="tühi",bg="lightsteelblue",command=lisa_suu)
+ch_suu.pack(side=LEFT)
+var_eyes=StringVar()
+ch_eyes=Checkbutton(aken,text="Silmad", variable=var_eyes, onvalue="Silmad", offvalue="tühi",bg="lightsteelblue",command=lisa_eyes)
+ch_eyes.pack(side=LEFT)
+var_nao=StringVar()
+ch_nao=Checkbutton(aken,text="Nägu", variable=var_nao, onvalue="Nägu", offvalue="tühi",bg="lightsteelblue",command=lisa_nao)
+ch_nao.pack(side=LEFT)
+c.pack(side=RIGHT)
+aken.mainloop()
